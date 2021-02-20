07d22fc6011b336ad90d4104c06c8774ac93207f
Zsolt Füzesi <zsolt.fuzesi@idomsoft.hu>
2021-02-20 12:39:26 +0100

    header and menu fixed

--- README.md
+++ README.md
@@ -22,3 +22,23 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Fejlesztés (így történt)
+
+# Font awesome telepítése
+```json
+  "dependencies": {
+    "@fortawesome/fontawesome-free": "^5.15.2",ű...
+    }
+```
+ezt követően hozzáadtam a szükséges beállításokat a main.ts-ben
+```html
+main.ts
+// fontawesome
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+```
+használata:
+```html
+<i class="fas fa-arrow-left"></i>
+```
--- src/App2.vue
+++ src/App2.vue
@@ -1,53 +1,87 @@
 <template>
-    <div class="light-blue-skin">
-        <mdb-side-nav slim :isCollapsed="collapsed" name="Anna Deynah" :OpenedFromOutside="true"
-                      logo="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" logoRound sideNavClass="sn-bg-1"
-                      mask="strong" hidden>
-            <mdb-side-nav-nav>
-                <mdb-side-nav-cat name="Submit blog" icon="chevron-right">
-                    <mdb-side-nav-item href="#" slimIcon="address-book">Submit listing</mdb-side-nav-item>
-                    <mdb-side-nav-item href="#" slimIcon="address-card">Registration form</mdb-side-nav-item>
-                </mdb-side-nav-cat>
+  <div class="light-blue-skin">
+    <mdb-side-nav
+      slim
+      :isCollapsed="collapsed"
+      name="Anna Deynah"
+      :OpenedFromOutside="true"
+      logo="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
+      logoRound
+      sideNavClass="sn-bg-1"
+      mask="strong"
+      hidden
+    >
+      <mdb-side-nav-nav>
+        <mdb-side-nav-cat name="Submit blog" icon="chevron-right">
+          <mdb-side-nav-item href="#" slimIcon="address-book"
+            >Submit listing</mdb-side-nav-item
+          >
+          <mdb-side-nav-item href="#" slimIcon="address-card"
+            >Registration form</mdb-side-nav-item
+          >
+        </mdb-side-nav-cat>
         <mdb-side-nav-cat name="Instruction" far icon="hand-pointer">
-                    <mdb-side-nav-item href="#" slimIcon="blog">For bloggers</mdb-side-nav-item>
-                    <mdb-side-nav-item href="#" slimIcon="pen">For authors</mdb-side-nav-item>
-                </mdb-side-nav-cat>
-                <mdb-side-nav-cat name="About" icon="eye">
-                    <mdb-side-nav-item href="#" slimIcon="handshake">Introduction</mdb-side-nav-item>
-                    <mdb-side-nav-item href="#" slimIcon="tasks">Monthly meetings</mdb-side-nav-item>
-                </mdb-side-nav-cat>
-                <mdb-side-nav-cat name="Contact me" far icon="envelope">
-                    <mdb-side-nav-item href="#">FAQ</mdb-side-nav-item>
-                    <mdb-side-nav-item href="#" slimIcon="paper-plane">Write a message</mdb-side-nav-item>
-                </mdb-side-nav-cat>
-                <mdb-side-nav-item icon="angle-double-left" open-icon="angle-double-right" @toggle="toggleSlim" toggler
-                                   :collapsed="collapsed" fixed>Minimize
-                </mdb-side-nav-item>
-            </mdb-side-nav-nav>
-        </mdb-side-nav>
-    </div>
+          <mdb-side-nav-item href="#" slimIcon="blog"
+            >For bloggers</mdb-side-nav-item
+          >
+          <mdb-side-nav-item href="#" slimIcon="pen"
+            >For authors</mdb-side-nav-item
+          >
+        </mdb-side-nav-cat>
+        <mdb-side-nav-cat name="About" icon="eye">
+          <mdb-side-nav-item href="#" slimIcon="handshake"
+            >Introduction</mdb-side-nav-item
+          >
+          <mdb-side-nav-item href="#" slimIcon="tasks"
+            >Monthly meetings</mdb-side-nav-item
+          >
+        </mdb-side-nav-cat>
+        <mdb-side-nav-cat name="Contact me" far icon="envelope">
+          <mdb-side-nav-item href="#">FAQ</mdb-side-nav-item>
+          <mdb-side-nav-item href="#" slimIcon="paper-plane"
+            >Write a message</mdb-side-nav-item
+          >
+        </mdb-side-nav-cat>
+        <mdb-side-nav-item
+          icon="angle-double-left"
+          open-icon="angle-double-right"
+          @toggle="toggleSlim"
+          toggler
+          :collapsed="collapsed"
+          fixed
+          >Minimize
+        </mdb-side-nav-item>
+      </mdb-side-nav-nav>
+    </mdb-side-nav>
+  </div>
 </template>
 <script>
-    import {mdbSideNav, mdbSideNavNav, mdbSideNavCat, mdbSideNavItem, waves} from 'mdbvue';
+import {
+  mdbSideNav,
+  mdbSideNavNav,
+  mdbSideNavCat,
+  mdbSideNavItem,
+  waves
+} from "mdbvue";
 
-    export default {
-        name: 'SideNavPage',
-        components: {
-            mdbSideNav,
-            mdbSideNavNav,
-            mdbSideNavCat,
-            mdbSideNavItem
-        },
-        data() {
-            return {
-                collapsed: false,
-            };
-        },
-        methods: {
-            toggleSlim() {
-                this.collapsed = !this.collapsed;
-            }
-        },
-        mixins: [waves]
+export default {
+  name: "SideNavPage",
+  components: {
+    mdbSideNav,
+    mdbSideNavNav,
+    mdbSideNavCat,
+    mdbSideNavItem
+  },
+  data() {
+    return {
+      collapsed: false
     };
+  },
+  methods: {
+    toggleSlim() {
+      this.collapsed = !this.collapsed;
+    }
+  },
+  mixins: [waves]
+};
 </script>
