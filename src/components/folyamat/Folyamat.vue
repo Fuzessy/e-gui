2506ac241f11508b767acecab0ca4926d9964000
Zsolt Füzesi <zsolt.fuzesi@idomsoft.hu>
2021-05-28 22:57:43 +0200

    folyamat frame introduced

---
+++ src/components/folyamat/Folyamat.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="folyamat-frame">
+    <div class="folyamat-header">
+      folyamat-header
+    </div>
+
+    <div class="folyamat-center">
+      <div class="folyamat-main">
+        <slot name="folyamat-main"></slot>
+      </div>
+      <div class="folyamat-buttons">
+        folyamat-buttons
+      </div>
+    </div>
+
+    <div class="folyamat-footer">
+      folyamat-footer
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class Folyamat extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+.folyamat-frame {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  .folyamat-header {
+    background-color: lightblue;
+  }
+
+  .folyamat-center {
+    display: flex;
+    flex-grow: 1;
+
+    .folyamat-main {
+      flex-grow: 1;
+      display: flex;
+      flex-direction: column;
+      background-color: $folyamat-background-color;
+    }
+
+    .folyamat-buttons {
+      background-color: #4e78ae;
+      min-width: 150px;
+    }
+  }
+
+
+  .folyamat-footer {
+    background-color: chocolate;
+    height: 30px;
+  }
+
+
+
+}
+
+</style>
--- src/views/tanulo-valasztas/TanuloValasztas.vue
+++ src/views/tanulo-valasztas/TanuloValasztas.vue
@@ -1,88 +1,75 @@
 <template>
-  <div class="folyamat-frame">
-    <div class="folyamat-header">
-      folyamat-header
-    </div>
-
-    <div class="folyamat-center">
-      <div class="folyamat-main">
-        <div class="folyamat-tablazat" ref="folyamat-tablazat">
-            <FuzTable
-                :headers="headers"
-                :rows="tanulok"
-                @row-selected="rowSelected"
-            />
-        </div>
-        <div class="splitter"
-             @mousedown="splitterMouseDown"
-        >
-          <i class="fas fa-grip-lines"></i>
-        </div>
-        <div class="folyamat-details">
-          <div
+  <folyamat>
+    <template v-slot:folyamat-main>
+      <div class="folyamat-tablazat" ref="folyamat-tablazat">
+        <FuzTable
+            :headers="headers"
+            :rows="tanulok"
+            @row-selected="rowSelected"
+        />
+      </div>
+      <div class="splitter"
+           @mousedown="splitterMouseDown"
+      >
+        <i class="fas fa-grip-lines"></i>
+      </div>
+      <div class="folyamat-details">
+        <div
             class="testMe"
             style="
                 text-align: left;
                 padding-left: 10px;
                 padding-top: 10px;
                     "
-          >
-            <div>
-              <div>Részletek</div>
-              <div style="
+        >
+          <div>
+            <div>Részletek</div>
+            <div style="
                 border-bottom: 1px solid rgba(128,138,255,0.27);
                 filter: blur(2px);
               "></div>
-            </div>
-
           </div>
-          <template v-if="selectedRow">
-            <div style="display: flex; flex-direction: row; width:100%">
-              <div style="flex-grow: 1">
-                <fuz-input
-                    label="Tanuló azonosító"
-                    disabled
-                    v-model="selectedRow.tanuloId"/>
-              </div>
-              <div style="flex-grow: 1">
-                <fuz-input
-                    label="Tanuló neve"
-                    disabled
-                    v-model="selectedRow.tanuloNev"/>
-              </div>
-            </div>
-            <div style="display: flex">
-              <div style="flex-grow: 1">
-                <fuz-input
-                    label="Csoport neve"
-                    disabled
-                    v-model="selectedRow.csoportNev"/>
-              </div>
-              <div style="flex-grow: 1">
-                <fuz-input
-                    label="Csoport vezető"
-                    disabled
-                    v-model="selectedRow.csoportVezetoNev"/>
-              </div>
-            </div>
-          </template>
-          <template v-else>
-            <p style="font-size: 36px">
-            <i class="far fa-times-circle"></i>
-            </p>
-            <p>Nincs kijelölt sor.</p>
-          </template>
-        </div>
-      </div>
-      <div class="folyamat-buttons">
-        folyamat-buttons
-      </div>
-    </div>
 
-    <div class="folyamat-footer">
-      folyamat-footer
-    </div>
-  </div>
+        </div>
+        <template v-if="selectedRow">
+          <div style="display: flex; flex-direction: row; width:100%">
+            <div style="flex-grow: 1">
+              <fuz-input
+                  label="Tanuló azonosító"
+                  disabled
+                  v-model="selectedRow.tanuloId"/>
+            </div>
+            <div style="flex-grow: 1">
+              <fuz-input
+                  label="Tanuló neve"
+                  disabled
+                  v-model="selectedRow.tanuloNev"/>
+            </div>
+          </div>
+          <div style="display: flex">
+            <div style="flex-grow: 1">
+              <fuz-input
+                  label="Csoport neve"
+                  disabled
+                  v-model="selectedRow.csoportNev"/>
+            </div>
+            <div style="flex-grow: 1">
+              <fuz-input
+                  label="Csoport vezető"
+                  disabled
+                  v-model="selectedRow.csoportVezetoNev"/>
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <p style="font-size: 36px">
+            <i class="far fa-times-circle"></i>
+          </p>
+          <p>Nincs kijelölt sor.</p>
+        </template>
+      </div>
+    </template>
+  </folyamat>
 </template>
 
 <script lang="ts">
@@ -92,9 +79,10 @@ import axios from "axios";
 import FuzTable from "@/components/table/FuzTable.vue";
 import FuzTableHeaderModel from "@/components/table/model/FuzTableHeaderModel";
 import FuzInput from "@/components/form/FuzInput.vue";
+import Folyamat from "@/components/folyamat/Folyamat.vue";
 
 @Component({
-  components: { FuzInput, FuzTable },
+  components: { Folyamat, FuzInput, FuzTable },
   beforeRouteEnter(to: Route, from: Route, next: Function): void {
     next();
   }
@@ -195,44 +183,13 @@ export default class TanuloValasztas extends Vue {
   color: $myprimary;
 }
 
-.folyamat-frame {
-  display: flex;
-  flex-direction: column;
+.folyamat-tablazat {
+  height: 200px;
+}
+
+.folyamat-details {
+  background-color: rgba(209, 210, 255, 0.23);
   height: 100%;
-}
-
-.folyamat-header {
-  background-color: lightblue;
-}
-
-.folyamat-center {
-  display: flex;
-  flex-grow: 1;
-}
-
-.folyamat-footer {
-  background-color: chocolate;
-  height: 30px;
-}
-
-.folyamat-main {
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-
-  .folyamat-tablazat {
-    height: 200px;
-  }
-
-  .folyamat-details {
-    background-color: rgba(209, 210, 255, 0.23);
-    height: 100%;
-  }
-}
-
-.folyamat-buttons {
-  background-color: #4e78ae;
-  min-width: 150px;
 }
 
 .splitter{
